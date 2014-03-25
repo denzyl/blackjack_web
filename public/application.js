2fb90c231c3301887b56506d6d5bc5b0d9441243
Denzyl Balram <denzyl.balram@gmail.com>
2014-03-24 21:45:12 -0700

    Fixed error on application.js

--- public/application.js
+++ public/application.js
@@ -17,7 +17,7 @@ function player_hit() {
 }
 
 function player_stay() {
-  $(document).on('click', 'form#hit_form input', function() {
+  $(document).on("click", "form#stay_form input", function() {
     $.ajax({
       type: 'POST',
       url: '/game/player/stay'
@@ -29,7 +29,7 @@ function player_stay() {
 }
 
 function dealer_hit() {
-  $(document).on('click', 'form#hit_form input', function() {
+  $(document).on("click", "form#dealer_hit input", function() {
     $.ajax({
       type: 'POST',
       url: '/game/dealer/hit'
